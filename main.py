34aa88691b214c5513dbd716acc365ac322ffc5f
Alexandre <alexandre.roberto9@hotmail.com>
2022-02-28 17:14:08 -0300

    adicionando a logica de excluir filmes na classe Db_movies e criando classe Author

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from datetime import datetime
-from sqlalchemy import Column, Date, Integer, Text, UniqueConstraint, create_engine, inspect, String, VARCHAR
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Date, Integer, Text, UniqueConstraint, create_engine, inspect, String, VARCHAR,ForeignKey
+from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 import os
 
+
 # Setting the Environment Variables!
-os.environ['Server_mssql'] = ''
-os.environ['Database_mssql'] = ''
-os.environ['Driver_mssql'] = ''
+os.environ['Server_mssql'] = 'DESKTOP-G8L8E1L'
+os.environ['Database_mssql'] = 'pythonSQL'
+os.environ['Driver_mssql'] = 'ODBC Driver 17 for SQL Server'
 
 # MSSLQ authentification
 # this login is based on Windows Authentification
@@ -33,12 +34,7 @@ Session = sessionmaker()
 
 
 # tables on database
-class User(Base):
-    __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
-    name = Column(VARCHAR(50), nullable=False)
-    UniqueConstraint(name)
 
 class Db_movies(Base):
     __tablename__ = 'Db_movies'
@@ -47,29 +43,60 @@ class Db_movies(Base):
     launch_date = Column(Date, nullable=True)
     movie_gender = Column(String, nullable=False)
     movie_description = Column(Text,nullable=True)
+    Author_id = Column(Integer, ForeignKey('Author.id'))
+    author = relationship("Author", back_populates="Movies")
     UniqueConstraint(movie_name)
+
+
+
     # function that append a movie in the database.
-    def append_movies(name,date,gender,description=''):
-        formated_date = datetime.strptime(date, '%d/%m/%Y').date()
-        session.add(Db_movies(movie_name = name, launch_date = formated_date, movie_gender = gender, movie_description = description))
+    # EXAMPLE: Db_movies.append_movies('Fight Club', '01/10/1990','action','test')
+    def append_movies(name,date,gender,author_id,description=''):
+        formated_date = datetime.strptime(date,'%d/%m/%Y').date()
+        session.add(Db_movies(movie_name = name, launch_date = formated_date, movie_gender = gender, movie_description = description, author = author_id))
         session.commit()
+
     # function that make a query in the database based on the parameter
-    def query(movie_name = ''):
+    def query(name = '',delete = False):
         arr = list()
-        if movie_name == '':
+        # EXAMPLE: x = Db_movies.query()
+        if name == '' and delete == False:
             for instance in session.query(Db_movies).all():
                 arr.append(instance.__dict__)
             return arr
 
-        elif len(movie_name) >= 1:
+        # EXAMPLE: x = Db_movies.query('Fight Club')
+        elif len(name) >= 1 and delete == False:
             arr = list()
-            for instance in session.query(Db_movies).filter(Db_movies.movie_name == movie_name)[:]:
+            for instance in session.query(Db_movies).filter(Db_movies.movie_name == name):
                 arr.append(instance.__dict__)
             return arr
 
+        elif delete == True and name != '':
+            for instance in session.query(Db_movies).filter(Db_movies.movie_name == name):
+                x= session.query(Db_movies).filter_by(movie_name=name).count()
+                session.delete(instance)
+                session.commit()
+                return x
+                
 
+class Author(Base):
+    __tablename__ = 'Author'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(VARCHAR(50), nullable=False)
+    last_name = Column(VARCHAR(50), nullable=False)
 
+    def append_author(name,last_name):
+        session.add(Author(first_name = name, last_name = last_name))
+        session.commit()
     
+
+
+Author.Movies = relationship(
+    "Db_movies", order_by=Db_movies.id_movie, back_populates="author"
+)
+
+
 
 
 # creates all customs classes as tables.
@@ -80,18 +107,22 @@ Session.configure(bind=engine)
 session = Session()
 
 
+
 # ---------------------------** -------------------------------** --------------------------------------**-------------
+
+
+Author.append_author('Chuck','Palahniuk')
 
 # Db_movies.append_movies('forrest gump','30/05/2014','Drama/Romance','''Slow-witted Forrest Gump (Tom Hanks) has never thought of himself as disadvantaged, and thanks to his supportive mother (Sally Field), he leads anything but a restricted life. Whether dominating on the gridiron as a college football star, fighting in Vietnam or captaining a shrimp boat, Forrest inspires people with his childlike optimism. But one person Forrest cares about most may be the most difficult to save -- his childhood love, the sweet but troubled Jenny (Robin Wright).
 # ''')
 
 
-Db_movies.query()
+# x = Db_movies.query('Fight Club')
+# print(x)
 
-# session.add(db_movies(id_movie='Fight Club', launch_date =dt.date(1990, 10, 1),movie_gender = 'action', movie_description = 'test'))
-# session.commit()
 
-# Db_movies.append_movies('Fight Club', '1990-10-01','action','test')
+
+# Db_movies.append_movies('Fight Club', '01/10/1990','action','test')
 
 # query = session.query(User.name, User.birthday)
 # for row in query:
